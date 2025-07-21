c20eb55af71fe73bf0f1bb85fc7c41d2a57e955e
XinAi22 <xinnnnn0202@gmail.com>
2025-07-21 06:43:05 +0000

    Added conclusion

---
+++ Part1/ex05.py
@@ -0,0 +1,11 @@
+# Word variables
+word1 = 'car '
+word2 = 'exceeded '
+text1 = 'speed limit '
+word3 = 'the '
+
+# Combine the words into one sentence and display on screen
+sentence = word3 + word1 + word2 + word3 + text1
+
+
+print(sentence)
