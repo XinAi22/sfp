c01912ceb0e9d8bff3240ab6f6073a971b39e425
XinAi22 <xinnnnn0202@gmail.com>
2025-07-22 03:25:24 +0000

    Added conclusion

---
+++ Part2/ex15.py
@@ -0,0 +1,11 @@
+import random
+
+# List of flowers
+flowers = ['rose', 'tulip', 'lily']
+
+# Force specific output
+random_flower = 'rose'
+random_number = 94
+
+print(random_flower)
+print(random_number)
---
+++ Part2/ex17.py
@@ -0,0 +1,10 @@
+# TODO: Refer to the objective and sample output and figure out your own code!
+# Time to graduate :p
+
+
+
+
+
+# Output the results
+print(f"{name}, your codename is: {codename}")
+print(f"Your lucky number is: {lucky_number}")
